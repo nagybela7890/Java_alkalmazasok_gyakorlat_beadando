a62082986c8a2fb735da7d41b60c6e5fa9880936
kristofcsontos <csontoskristof@gmail.com>
2022-11-24 12:26:35 +0100

    urlap2
    urlap pdf

--- beadando/src/main/java/com/example/beadando/UrlapController.java
+++ beadando/src/main/java/com/example/beadando/UrlapController.java
@@ -1,18 +1,24 @@
 package com.example.beadando;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
 @Controller
 public class UrlapController {
     @GetMapping("/urlap1")
     public String urlapForm(Model model) {
-        model.addAttribute("attr1", new UzenetOsztaly());
+        model.addAttribute("uzenetOsztaly", new UzenetOsztaly());
         return "urlap";
     }
     @PostMapping("/feladat2")
-    public String urlapSubmit(@ModelAttribute UzenetOsztaly uzenetOsztaly, Model model) {
+    public String urlapSubmit(@Valid @ModelAttribute UzenetOsztaly uzenetOsztaly, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors())
+            return "urlap";
         model.addAttribute("attr2", uzenetOsztaly);
         return "eredmeny";
     }
